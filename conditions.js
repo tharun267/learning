5ebf4d86c55b4bec80d1647312acec96bcb926ff
Tharun Bathula <tharun267@gmail.com>
2023-12-08 18:00:00 +0100

    Added conditions file

---
+++ conditions.js
@@ -0,0 +1,86 @@
+// var is like a glass can fill anything into it
+// if condition
+
+var a = 10; // numbers nimpinam
+var b = 20;
+
+var c = 100;
+var d = 200;
+
+var e = d > c; // true - yes(true), no(false) nimpinam
+
+var f = "Tharun" // string (collection of chars) characters = "T", "h", "a", "r", "u", "n", "<", ">"
+
+
+// OPERATORS
+
+// &&  AND operator
+// ||  OR operator
+// > greater than
+// < less than
+// <= less than or equal
+// >= greater than or equal
+
+console.log("Value of e", e)
+
+console.log(f);
+
+// logical operators 
+
+// (AND) == (&&) - used in coding // rich
+
+// true AND true = true
+// true AND false = false
+// false AND true = false
+// false AND false = false
+
+// (OR) == (||) - used in codeing // poor
+
+// true OR false = true
+// false OR false = false
+// false OR true = true
+// true OR true = true
+
+var value = a>b || e;
+
+if(value) {
+    // dabba - 1
+    // correct aythe 
+    // ikkadiki vasthadhi
+    console.log("I Am Great")
+} else {
+    // dabba - 2 
+    // wrong aythe
+    // lekapothe ikkadiki vasthadhi
+    console.log("I Am Less")
+}
+
+
+var s = "T" // character (char)
+var s1 = "h" // character (char)
+var s2 = s + s1; // charcter + character + ... n characters = string
+
+var s3 = s2 + ">";
+
+console.log(s2);
+
+console.log(s3); // output = "Th>"
+
+
+// comments are of two types
+// single line comments
+// multi line comments
+
+// Example: this is a single line comment
+
+/*
+hjfcsjhgfv*/
+
+/*
+
+******
+****\***
+This is a multiline comment
+
+
+*/
