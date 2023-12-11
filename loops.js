91c52f4e886531f168d692733ec731aeb043b967
Tharun Bathula <tharun267@gmail.com>
2023-12-11 09:40:52 +0100

    Loops

---
+++ loops.js
@@ -0,0 +1,27 @@
+// calander days
+// 1 - 30
+
+// Print the number of days in a month?
+// console.log(1,2,3,4,5,6,) // incrementing by 1
+// sequence by 1  (conditioning) oka dani tharuvata okati
+// starting from 1;
+
+// loop
+//  1) starting point (initialization)
+//  2) condition (ekkadivaraki - 30)
+//  3) increment 
+
+// var i = 1;
+// console.log(i);
+
+// i = i + 1
+// console.log(i);
+
+// i = i + 1; // i = i++;
+// console.log(i);
+
+// Only one starting point like cinema
+//  Deepak idhi (1); Pavan idhi (2); Manoj idhi (4)
+for(var i = 1; i <= 10; i = i + 2) {
+    console.log(i); // Sweety idhi (dabba lopaliki vasthadhi) (3)
+}
